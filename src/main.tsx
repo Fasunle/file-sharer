523fa1fe26c39ba2de70beb31d298666ad7036fc
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-11-03 11:52:40 +0100

    feat: setup tailwindcss and react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as AppRouter } from 'react-router-dom';
+import App from './App';
+import './styles/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+  <AppRouter>
     <App />
-  </React.StrictMode>
-)
+  </AppRouter>,
+);
