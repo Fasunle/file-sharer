def3717feb46728b0dd7d15cdcef7919a7d321b2
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-11-04 13:05:14 +0100

    feat: Add password reset functionality

--- src/components/Auth/index.tsx
+++ src/components/Auth/index.tsx
@@ -1,18 +1,39 @@
-import {useLocation} from 'react-router-dom';
+import {useState} from 'react';
+import {Link} from 'react-router-dom';
+import ForgotPassword from './ForgotPassword';
+import Login from './Login';
 
 export default function Auth() {
-  const location = useLocation();
+  const [forgotPassword, setForgotPassword] = useState(false);
   return (
     <main className='auth--container'>
       <div className='auth__card--container'>
         <div>
           <h2>Have an account&#63;</h2>
-          <button className='btn btn--cta'>Login Here</button>
+          <Link to='/auth/login'>
+            <button
+              className='btn btn--cta'
+              onClick={() => setForgotPassword(false)}
+            >
+              Login Here
+            </button>
+          </Link>
         </div>
         <div>
           <h2>Forgot Password&#63;</h2>
-          <button className='btn btn--cta'>Click Here</button>
+          <Link to='/auth/reset'>
+            <button
+              className='btn btn--cta'
+              onClick={() => setForgotPassword(true)}
+            >
+              Click Here
+            </button>
+          </Link>
         </div>
+      </div>
+
+      <div className={`overlay ${forgotPassword ? 'to-right' : ''}`}>
+        {!forgotPassword ? <Login /> : <ForgotPassword />}
       </div>
     </main>
   );
