855922256ebf5150c046899921c1a8643c2fe88c
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-11-04 02:18:08 +0100

    feat: card plane markup

--- src/components/Auth/index.tsx
+++ src/components/Auth/index.tsx
@@ -1,11 +1,19 @@
 import {useLocation} from 'react-router-dom';
 
-import Login from './Login';
-import Signup from './Signup';
-
 export default function Auth() {
   const location = useLocation();
   return (
-    <main>{location.pathname === '/auth/signup' ? <Signup /> : <Login />}</main>
+    <main className='auth--container'>
+      <div className='auth__card--container'>
+        <div>
+          <h2>Have an account&#63;</h2>
+          <button className='btn btn--cta'>Login Here</button>
+        </div>
+        <div>
+          <h2>Forgot Password&#63;</h2>
+          <button className='btn btn--cta'>Click Here</button>
+        </div>
+      </div>
+    </main>
   );
 }
