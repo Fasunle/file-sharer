582541d0f60d1612a5c3af46fbd54d07ed68d8b1
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-11-05 10:53:29 +0100

    feat/aside: recently shared file list

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,8 +1,12 @@
 import {useState} from 'react';
 import Header from '../Header';
+import FileCard from './FileCard';
+import {IFileCardProps} from './interface';
 
 export default function Home() {
   const [isOpen, setIsOpen] = useState(false);
+
+  const files: IFileCardProps[] = [];
   return (
     <section className='home'>
       <Header setIsOpen={setIsOpen} isOpen={isOpen} />
@@ -13,6 +17,20 @@ export default function Home() {
             <h2>Kehinde Fasunle</h2> <span></span>
           </li>
           <li className='email'>kfasunle@gmail.com</li>
+        </ul>
+
+        <ul className='file__share'>
+          <h3 className='title'>Recently Shared</h3>
+          <li className='files'>
+            {files.map((file) => (
+              <FileCard
+                key={file.name}
+                name={file.name}
+                size={file.size}
+                type={file.type}
+              />
+            ))}
+          </li>
         </ul>
       </aside>
     </section>
