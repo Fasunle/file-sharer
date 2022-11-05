ef10fade1413b0bdd5cb6c9bdf26651b708b5b8b
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-11-05 07:58:31 +0100

    feat: update Home component

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,3 +1,15 @@
+import {useState} from 'react';
+import Header from '../Header';
+
 export default function Home() {
-  return <h1 className='font-mono'>Home Page</h1>;
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <section className='home'>
+      <Header setIsOpen={setIsOpen} isOpen={isOpen} />
+      <main className='home__content'>Main</main>
+      <aside className={`home__sidebar ${isOpen && 'show-sidebar'}`}>
+        Aside
+      </aside>
+    </section>
+  );
 }
