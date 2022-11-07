e3e0fae13f3bf4a987db404fb8039ed45070facb
Kehinde Hussein, Fasunle <kfasunle@gmail.com>
2022-11-07 14:55:59 +0100

    feat: setup react-query with the project

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter as AppRouter} from 'react-router-dom';
+import {QueryClientProvider, QueryClient} from 'react-query';
 import App from './App';
 import './styles/main.scss';
 
+const queryClient = new QueryClient();
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <AppRouter>
-    <App />
-  </AppRouter>,
+  <QueryClientProvider client={queryClient}>
+    <AppRouter>
+      <App />
+    </AppRouter>
+  </QueryClientProvider>,
 );
